d09dde06ef955a5e9f9b38f9efd6c06426256913
Paramita Debbarma <paramitadebbarma404@gmail.com>
2025-06-12 23:38:54 +0530

    folder update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Greenie Chat',
+    version = '0.0.0',
+    author = 'Paramita',
+    author_email = 'paramitadebbarma404@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+) 
